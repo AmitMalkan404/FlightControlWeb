98e829b2a84bd4f86284f3531d72a0eeb9be1e19
nivnis <nivhaim.nisani@live.biu.ac.il>
2020-05-17 23:24:35 +0300

    refresh flights try

---
+++ FlightControlWeb/wwwroot/Scripts/TableUpdates.js
@@ -0,0 +1,45 @@
+﻿//let allMyFlightsUrl = "https://localhost:5001/api/Flights?relative_to=2020-12-27T02:00:30Z&sync_all";
+//$.getJSON(allMyFlightsUrl, function (data) {
+//    data.forEach(function (flight) {
+//            ("#myflightstable").append("<tr><td>" +
+//                flight.FlightId +
+//                "</td>" +
+//                "<td>" +
+//                flight.CompanyName +
+//                "</td></tr>" >
+//            );
+//        });
+//    });
+
+//let allMyFlightsUrl = "https://localhost:5001/api/Flights?relative_to=2020-12-27T02:00:30Z&sync_all";
+//$.getJSON(allMyFlightsUrl, function(data) {
+//    data.forEach(function(flight) {
+//        $("#myflightstable").append("<tr><td>") +
+//            flight.FlightId +
+//            "</td>" +
+//            "<td>" +
+//            flight.CompanyName +
+//            "</td></tr>";
+//    });
+//});
+
+//const flightsBody = document.querySelector("#myflightstable")
+
+
+let allMyFlightsUrl = "https://localhost:5001/api/Flights?relative_to=2020-12-27T02:00:30Z&sync_all";
+
+function getAllFlights() {
+    $.getJSON(allMyFlightsUrl, function (data) {
+        //data.forEach(function (flight) {
+        //    $("#myflightstable").append("<tr><td>") +
+        //        flight.FlightId +
+        //        "</td>" +
+        //        "<td>" +
+        //        flight.CompanyName +
+        //        "</td></tr>";
+        //});
+        console.log(data);
+    });
+}
+
+setInterval(getAllFlights, 3000);
