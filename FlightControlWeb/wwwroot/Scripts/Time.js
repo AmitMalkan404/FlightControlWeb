0d86d1a429ec8784c3f466a06b2c23623b8227fb
nivnis <nivhaim.nisani@live.biu.ac.il>
2020-05-29 01:20:31 +0300

    time eslint works

--- FlightControlWeb/wwwroot/Scripts/Time.js
+++ FlightControlWeb/wwwroot/Scripts/Time.js
@@ -1,12 +1,15 @@
-﻿let allMyFlightsUrl;
+﻿
+/* eslint-disable no-unused-vars */
+let allMyFlightsUrl;
+/* eslint-enable no-unused-vars */
 
 function getTime() {
-    let d = new Date();
-    let utcString = d.toUTCString();
+    const d = new Date();
+    const utcString = d.toUTCString();
     let time = new Date(utcString);
     time = time.toISOString().slice(0, -2);
-    //console.log(time);
-    allMyFlightsUrl = "https://localhost:5001/api/Flights?relative_to=" + time + "Z&sync_all";
+    // console.log(time);
+    allMyFlightsUrl = `https://localhost:5001/api/Flights?relative_to=${time}Z&sync_all`;
 }
 
 setInterval(getTime, 500);
