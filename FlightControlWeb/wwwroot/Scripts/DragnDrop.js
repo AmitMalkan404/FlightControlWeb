1a9cf71649af82e795e751a09a991abd2bf7ed28
nivnis <nivhaim.nisani@live.biu.ac.il>
2020-06-01 01:31:30 +0300

    post error handeling improved

--- FlightControlWeb/wwwroot/Scripts/DragnDrop.js
+++ FlightControlWeb/wwwroot/Scripts/DragnDrop.js
@@ -29,22 +29,45 @@ function unhighlight(e) {
 /* eslint-disable no-prototype-builtins */
 function uploadFile(file) {
     const url = 'https://localhost:5001/api/FlightPlans';
-
-    fetch(url,
-        {
-            method: 'POST',
-            body: file
-        })
-        .then((response) => response.json())
-        .then((data) => {
-            for (const err in data.errors) {
-                if (data.errors.hasOwnProperty(err)) {
-                    // console.log(data.errors[err].toString());
-                    PostErrorNotification(data.errors[err].toString());
+    try {
+        (async () => {
+            const rawResponse = await fetch(url, {
+                method: 'POST',
+                body: file
+            });
+            if (rawResponse.ok) {
+                console.log('no errors');
+                return;
+            } else {
+                const jsonContent = await rawResponse.json();
+                for (const err in jsonContent.errors) {
+                    if (jsonContent.errors.hasOwnProperty(err)) {
+                        // console.log(data.errors[err].toString());
+                        PostErrorNotification(jsonContent.errors[err].toString());
+                        throw Error('e');
+                    }
                 }
             }
-        })
-        .catch((err) => {console.log(err);});
+        })();
+    } catch (err) {
+        console.log('error: ' + err.message);
+    }
+
+    //fetch(url,
+    //    {
+    //        method: 'POST',
+    //        body: file
+    //    })
+    //    .then((response) => response.json())
+    //    .then((data) => {
+    //        for (const err in data.errors) {
+    //            if (data.errors.hasOwnProperty(err)) {
+    //                // console.log(data.errors[err].toString());
+    //                PostErrorNotification(data.errors[err].toString());
+    //            }
+    //        }
+    //    })
+    //    .catch((err) => {console.log(err);});
 }
 /* eslint-enable no-prototype-builtins */
 
