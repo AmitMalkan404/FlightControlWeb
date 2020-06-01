3ac273e161ad0b425a2ab23d910bb2abfe88a8f7
Amit Malkan <amit.malkan@gmail.com>
2020-06-01 22:18:31 +0300

    fixed the unit test PULL ONLY THE UNIT TEST PROJECT! DO NOT PULL THE FlightPlansControllers!!!!!!

--- FlightControllWebTests2/Controllers/FlightPlansControllerTest.cs
+++ FlightControllWebTests2/Controllers/FlightPlansControllerTest.cs
@@ -15,6 +15,7 @@ namespace FlightControllWeb.Controllers
     [TestClass()]
     public class FlightsControllerTests
     {
+        // Creating the DBContext and the Controller
         FlightContext _FlightDBContextMock;
         private readonly FlightPlansController fpc;
         public FlightsControllerTests()
@@ -28,24 +29,24 @@ namespace FlightControllWeb.Controllers
         }
 
         [TestMethod()]
-        public async Task GetFlightPlanTest()
+        public async Task PostFlightPlanTest()
         {
-            //FlightsControllerTests controllerTests = new FlightsControllerTests();
 
             // Assign
+
             InitialLocation location = new InitialLocation();
-            DateTime dateTime = new DateTime(2020, 5, 31, 18, 0, 0);
+            DateTime dateTime = new DateTime(2020, 5, 30, 18, 0, 0);
             location.DateTime = dateTime;
             location.Longitude = 30.522;
             location.Latitude = 34.943;
             Segment seg1 = new Segment();
-            seg1.FlightId = "LY1255";
+            seg1.FlightId = "EL-0123";
             seg1.Longitude = 33.567;
             seg1.Latitude = 31.865;
             seg1.TimespanSeconds = 180;
 
             Segment seg2 = new Segment();
-            seg2.FlightId = "LY1255";
+            seg2.FlightId = "EL-0123";
             seg2.Longitude = 35.674;
             seg2.Latitude = 30.651;
             seg2.TimespanSeconds = 720;
@@ -54,10 +55,9 @@ namespace FlightControllWeb.Controllers
             segList.Add(seg1);
             segList.Add(seg2);
 
-            //FlightPlan flightPlan1 = new FlightPlan("LY1255", 168, "EL-AL", location, segList, false);
             FlightPlan flightPlan1 = new FlightPlan();
             flightPlan1.Id = 1255;
-            flightPlan1.FlightId = "EL01";
+            flightPlan1.FlightId = "EL-0123";
             flightPlan1.Passengers = 168;
             flightPlan1.CompanyName = "EL-AL";
             flightPlan1.InitialLocation = location;
@@ -65,24 +65,21 @@ namespace FlightControllWeb.Controllers
             flightPlan1.IsExternal = false;
 
 
-            //_FlightDBContextMock.Add(flightPlan1);
-            //controllerTests.fpc.PostFlightPlan(flightPlan1);
-            Task<ActionResult<FlightPlan>> apiFlight = fpc.PostFlightPlan(flightPlan1);
-            //var hey = apiFlight;
-
-            //var contextFlight = await _FlightDBContextMock.FlightItems.FindAsync(1255);
-            var contextFlights = await _FlightDBContextMock.FlightItems.ToListAsync();
-            var contextFlight = contextFlights.Where(a => a.FlightId.CompareTo("EL01") == 0).First();
-            //var singleContextFlight = contextFlight[0];
             // Act
-            //Task<ActionResult<FlightPlan>> apiFlight = fpc.GetFlightPlan("LY1255");
+
+            double initialLatitude = 34.943;
+            Task<ActionResult<FlightPlan>> apiFlight = fpc.PostFlightPlan(flightPlan1);
+            var contextFlights = await _FlightDBContextMock.FlightItems.ToListAsync();
+            var contextFlight = contextFlights.Where(a => a.FlightId.CompareTo("EL-0123") == 0).First();
+
 
             //Assert
+
             Assert.IsNotNull(contextFlight);
-            Assert.IsTrue(contextFlight.FlightId.CompareTo("EL01")==0);
+            Assert.IsTrue(contextFlight.FlightId.CompareTo("EL-0123") ==0);
             Assert.IsTrue(contextFlight.Passengers == 168);
-            //FlightPlan flightPlan = await _context.FlightItems.Include(x => x.SegmentsList).Include(x => x.InitialLocation).Where(x => x.FlightId == id).FirstOrDefaultAsync();
-            //Assert.Fail();
+            Assert.AreEqual(segList, contextFlight.SegmentsList);
+            Assert.AreEqual(contextFlight.InitialLocation.Latitude, initialLatitude);
         }
     }
 
