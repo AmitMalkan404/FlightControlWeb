4d2a3d3126088014c0b2effc73d46466686e3404
nivnis <nivhaim.nisani@live.biu.ac.il>
2020-06-01 15:37:32 +0300

    small js file organization

---
+++ FlightControlWeb/wwwroot/Scripts/Flight.js
@@ -0,0 +1,21 @@
+﻿class Flight {
+    constructor(id, lat, lon, company, passengers) {
+        this.id = id;
+        this.latitude = lat;
+        this.longitude = lon;
+        this.iconExists = false;
+        this.airline = company;
+        this.planeMarker = null;
+        this.tableRow = null;
+        this.isExternal = null;
+        this.passengers = passengers;
+    }
+
+    setPlaneMarker(marker) {
+        this.planeMarker = marker;
+    }
+
+    getPlaneMarker() {
+        return this.planeMarker;
+    }
+}
--- FlightControlWeb/wwwroot/Scripts/dataHandler.js
+++ FlightControlWeb/wwwroot/Scripts/dataHandler.js
@@ -1,26 +1,4 @@
-﻿class Flight {
-    constructor(id, lat, lon, company, passengers) {
-        this.id = id;
-        this.latitude = lat;
-        this.longitude = lon;
-        this.iconExists = false;
-        this.airline = company;
-        this.planeMarker = null;
-        this.tableRow = null;
-        this.isExternal = null;
-        this.passengers = passengers;
-    }
-
-    setPlaneMarker(marker) {
-        this.planeMarker = marker;
-    }
-
-    getPlaneMarker() {
-        return this.planeMarker;
-    }
-}
-
-
+﻿
 const flightsArray = [];
 let myFlightPlanUrl;
 // let latlngs = new Array();
